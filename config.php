2383a3089c7c315a394fd0f94ec906a00789a5f6
Lennon Chan <lennonandjune@gmail.com>
2017-11-05 20:31:01 +0800

    init 2.3.0.3_rc

---
+++ config.php
@@ -0,0 +1,38 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://' . $_SERVER['HTTP_HOST'] . '/');
+
+// HTTPS
+define('HTTPS_SERVER', HTTP_SERVER);
+
+// DIR
+define('DIR_APPLICATION', $_SERVER['DOCUMENT_ROOT'] . '/catalog/');
+define('DIR_SYSTEM', $_SERVER['DOCUMENT_ROOT'] . '/system/');
+define('DIR_IMAGE', $_SERVER['DOCUMENT_ROOT'] . '/image/');
+define('DIR_LANGUAGE', $_SERVER['DOCUMENT_ROOT'] . '/catalog/language/');
+define('DIR_TEMPLATE', $_SERVER['DOCUMENT_ROOT'] . '/catalog/view/theme/');
+define('DIR_CONFIG', $_SERVER['DOCUMENT_ROOT'] . '/system/config/');
+define('DIR_CACHE', $_SERVER['DOCUMENT_ROOT'] . '/system/storage/cache/');
+define('DIR_DOWNLOAD', $_SERVER['DOCUMENT_ROOT'] . '/system/storage/download/');
+define('DIR_LOGS', $_SERVER['DOCUMENT_ROOT'] . '/system/storage/logs/');
+define('DIR_MODIFICATION', $_SERVER['DOCUMENT_ROOT'] . '/system/storage/modification/');
+define('DIR_UPLOAD', $_SERVER['DOCUMENT_ROOT'] . '/system/storage/upload/');
+
+// DB
+if (isset($_SERVER['LOCAL_DEV']) && $_SERVER['LOCAL_DEV'] == 'TEST') {
+    define('DB_DRIVER', 'mysqli');
+    define('DB_HOSTNAME', '127.0.0.1');
+    define('DB_USERNAME', 'root');
+    define('DB_PASSWORD', 'root');
+    define('DB_DATABASE', basename(__DIR__));
+    define('DB_PORT', '3306');
+    define('DB_PREFIX', 'oc_');
+} else {
+    define('DB_DRIVER', 'mysqli');
+    define('DB_HOSTNAME', '');
+    define('DB_USERNAME', '');
+    define('DB_PASSWORD', '');
+    define('DB_DATABASE', basename(__DIR__));
+    define('DB_PORT', '3306');
+    define('DB_PREFIX', 'oc_');
+}
